a844067e2f66045096635e4a5e44f6dfd9d70f8b
Erika Hughes <ehughes@knights.ucf.edu>
2018-07-26 19:37:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,25 +29,27 @@ function checkCollision(rock) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
 
     // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
-    const dodgerRightEdge = 0;
+    const dodgerRightEdge = dodgerLeftEdge + 40;
 
     const rockLeftEdge = positionToInteger(rock.style.left)
 
     // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
-    const rockRightEdge = 0;
+    const rockRightEdge = rockLeftEdge + 20;
 
-    if (false /**
-               * Think about it -- what's happening here?
-               * There's been a collision if one of three things is true:
-               * 1. The rock's left edge is < the DODGER's left edge,
-               *    and the rock's right edge is > the DODGER's left edge;
-               * 2. The rock's left edge is > the DODGER's left edge,
-               *    and the rock's right edge is < the DODGER's right edge;
-               * 3. The rock's left edge is < the DODGER's right edge,
-               *    and the rock's right edge is > the DODGER's right edge
-               */) {
-      return true
-    }
+   return rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge || rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge || rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge
+
+    //if (false /**
+              // * Think about it -- what's happening here?
+              // * There's been a collision if one of three things is true:
+              // * 1. The rock's left edge is < the DODGER's left edge,
+              // *    and the rock's right edge is > the DODGER's left edge;
+              // * 2. The rock's left edge is > the DODGER's left edge,
+              // *    and the rock's right edge is < the DODGER's right edge;
+              // * 3. The rock's left edge is < the DODGER's right edge,
+              // *    and the rock's right edge is > the DODGER's right edge
+              // */) {
+     // return true
+    //}
   }
 }
 
@@ -79,6 +81,12 @@ function createRock(x) {
      * If a rock collides with the DODGER,
      * we should call endGame()
      */
+     
+     if (checkCollision(rock)) {
+       endGame()
+     } else if (positionToInteger(rock.style.bottom) > 400) {
+       moveRock()
+     }
 
     /**
      * Otherwise, if the rock hasn't reached the bottom of
