87c3a16a8563221c0d33257ffccfc805c8d103d7
Erika Hughes <ehughes@knights.ucf.edu>
2018-07-26 20:14:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -162,6 +162,7 @@ function moveDodgerLeft() {
    */
    var left_side = positionToInteger(DODGER.style.left)
    window.requestAnimationFrame(() =>  left_side -= 4)
+   return left_side
 }
 
 function moveDodgerRight() {
