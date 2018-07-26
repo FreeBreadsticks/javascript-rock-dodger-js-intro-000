78d6277a84d89f0c4e88ed74cc8d6f324597cade
Erika Hughes <ehughes@knights.ucf.edu>
2018-07-26 20:05:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -138,7 +138,7 @@ function moveDodger(e) {
     moveDodgerLeft()
   }else if(e.which === RIGHT_ARROW){
     moveDodgerRight()
-  } else {
+  } else if() {
     return false
   }
   // implement me!
