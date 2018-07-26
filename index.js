c46226de0587c32f8ee6d801c9bb5e81b607b7a6
Erika Hughes <ehughes@knights.ucf.edu>
2018-07-26 20:03:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -133,9 +133,12 @@ function endGame() {
 }
 
 function moveDodger(e) {
+  e.preventDefault()
   if(e.which === LEFT_ARROW){
     moveDodgerLeft()
-  }else if
+  }else if(e.which === RIGHT_ARROW){
+    moveDodgerRight()
+  }
   // implement me!
   /**
    * This function should call `moveDodgerLeft()`
